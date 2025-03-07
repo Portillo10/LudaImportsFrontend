e96d74b9c45e98a612a38d8ae27247e09e90c603
Portillo <portillon02@gmail.com>
2025-03-07 00:35:37 -1000

    any

--- src/pages/Publisher/PendingPosting.tsx
+++ src/pages/Publisher/PendingPosting.tsx
@@ -3,7 +3,7 @@ import PendingTable from "./Tables/PendingPubsTable";
 
 function PendingPostingPage() {
   return (
-    <div className="basicContainer pt-4">
+    <div className="basicContainer pt-4 pb-8">
       <div className="flex flex-col gap-4 w-full justify-center items-center">
         <InProgressTable />
         <PendingTable />
