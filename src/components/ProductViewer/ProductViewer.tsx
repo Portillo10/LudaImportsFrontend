d7666f6e6a55d1a530eb9ade21acaea6098f4409
Portillo <portillon02@gmail.com>
2025-07-20 16:19:18 -0500

    todo good

--- src/components/ProductViewer/ProductViewer.tsx
+++ src/components/ProductViewer/ProductViewer.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { Item, ItemAttribute } from "../../types/item";
 import AttributesViewer from "./AttributesViewer";
 import ImageViewer from "./ImageViewer";
 import ProductInfoViewer from "./ProductInfoViewer";
+import Spinner from "../Spinner/Spinner";
 
 type ProductViewerProps = {
   product: Item | null;
@@ -16,6 +18,14 @@ const ProductViewer: React.FC<ProductViewerProps> = ({
   onChangeProduct,
   onChangeAttribute,
 }) => {
+  const [postingLoading, setPostingLoading] = useState<boolean>(false);
+
+  const onClickPublish = async () => {
+    setPostingLoading(true);
+    await onPublish(product);
+    setPostingLoading(false);
+  };
+
   if (product)
     return (
       <div className="flex flex-col gap-12 px-6 pt-4 items-end">
@@ -44,10 +54,10 @@ const ProductViewer: React.FC<ProductViewerProps> = ({
           />
         </div>
         <button
-          onClick={() => onPublish(product)}
-          className="bg-cyan-800 hover:bg-cyan-900 py-2 px-3 transition-all rounded-md sticky bottom-3 w-min"
+          onClick={() => onClickPublish()}
+          className={`${postingLoading ? "bg-cyan-900 cursor-default" : "bg-cyan-800"} hover:bg-cyan-900 py-2 px-3 transition-all rounded-md sticky bottom-3 flex items-center justify-center w-24`}
         >
-          Publicar
+          {postingLoading ? <Spinner size={24} /> : "Publicar"}
         </button>
       </div>
     );
